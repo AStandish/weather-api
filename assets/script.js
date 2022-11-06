be74b8f39d0c793d96a60f0fb20ae3795417d9b9
AStandish <112442942+AStandish@users.noreply.github.com>
2022-11-06 16:35:45 -0500

    added fetchbutton, fetch, requesturl, functions

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+const fetchButton = document.getElementById("fetch-button");
+
+function getApi(input) {
+  // fetch request gets a list of all the repos for the node.js organization
+  const requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&appid=a74bf83c5240b68543e1753ef9d16b93`;
+  fetch(requestUrl)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      getWeather(data[0]);
+    });
+}
+
+function getWeather(location) {
+  const lat = location.lat;
+  const lon = location.lon;
+  const requestUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=a74bf83c5240b68543e1753ef9d16b93`;
+  fetch(requestUrl)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
+
+getApi("London");
+
+fetchButton.addEventListener("click", getApi);
